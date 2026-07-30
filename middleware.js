3221f1e51d65b29ced7160621a1f0bf88157cbdf
Sagar Hc <ssag07906@example.com>
2026-07-30 19:57:16 +0530

    new updates

--- middleware.js
+++ middleware.js
@@ -1,7 +1,14 @@
 module.exports.isLoggedIn = (req ,res ,next)=>{
     if(!req.isAuthenticated()){
+        req.session.redirectUrl =req.originalUrl;
         req.flash("error","please login .");
         return res.redirect("/login");
     }
     next();   
-}
+};
+//this saves any redirect url into the locals
+module.exports.saveRedirectUrl = (req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }next();
+};
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -15,8 +16,13 @@ router.post("/signup",wrapAsync(async (req,res)=>{
     const newUser = new User({ email, username});
     const registeredUser = await User.register(newUser,password);
     console.log(registeredUser);
-    req.flash("success","welxome to wanderlust");
-    res.redirect("/listings");
+    req.login(registeredUser,(err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","welcome to wanderlust");
+    res.redirect("/listings"); 
+    })
     }catch(e){
         req.flash("error",e.message);
         res.redirect("/signup");
@@ -28,16 +34,19 @@ router.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 });
 
-router.post("/login",passport.authenticate("local",{
+router.post("/login",
+    saveRedirectUrl,
+    passport.authenticate("local",{
     failureRedirect :"/login",
     failureFlash:true,
   }),
   async(req,res)=>{
     req.flash("success","yo u are logged in buddy");
-    res.redirect("/listings"); 
+    res.redirect(res.locals.redirectUrl); 
   }
 );
 
+//logout route
 router.get("/logout",(req , res, next)=>{
     req.logout((err)=>{
     if(err){
